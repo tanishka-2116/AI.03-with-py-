91617dbf3b3c99773d7208134ff954497e64d2e0
tanishka-2116 <tanishkadhumal2006@gmail.com>
2026-01-18 11:39:29 +0530

    Added simple NumPy example

---
+++ numpy_example.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+# create a NumPy array
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Array:", numbers)
+print("Sum:", numbers.sum())
+print("Mean:", numbers.mean())
+print("Maximum:", numbers.max())
+print("Minimum:", numbers.min())
